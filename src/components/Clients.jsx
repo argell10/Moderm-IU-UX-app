9bca3894ba3a102c6f668ec969bc7eecc15e3286
Jhosua Rengel <105228140+argell10@users.noreply.github.com>
2022-12-28 15:37:45 -0500

    Project end (#13)
    * developer_hero (#2)
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * Developer section starts (#3)
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * Development section Business
    
    * Development section Business (#4)
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * refactoring files
    
    * Dev billing (#5)
    
    * Development section Business
    
    * refactoring files
    
    * Development Billing section
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * Bug (#6)
    
    * Development section Business
    
    * refactoring files
    
    * bug
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * Resolve conflicts
    
    * Development Section Testimonials (#7)
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * Dev testimonials (#8)
    
    * Development Section Testimonials
    
    * Development section Testimonials
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * Development clients section (#9)
    
    * Development Section Testimonials
    
    * Development section Testimonials
    
    * Development section clients
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * Dev cta (#10)
    
    * Development Section Testimonials
    
    * Development section Testimonials
    
    * Development section clients
    
    * Development section CTA
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * Dev footer (#11)
    
    * Development Section Testimonials
    
    * Development section Testimonials
    
    * Development section clients
    
    * Development section CTA
    
    * Developmet section Footer
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    * add styles in  section navbar (#12)
    
    * Development Section Testimonials
    
    * Development section Testimonials
    
    * Development section clients
    
    * Development section CTA
    
    * Developmet section Footer
    
    * add styles hover in section navbar
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>
    
    Co-authored-by: Jhosua <iris@pop-os.localdomain>

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import {clients} from '../constants'
+import styles from '../constants/style'
 
-function Clients() {
-  return (
-    <div>
-      Clients
-    </div>
-  )
-}
+const Clients = () => (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+            <img src={client.logo} alt="client" className='object-contain sm:w-[192px] w-[100px]' />
+          </div>
+        ))}
+      </div>
+    </section>
+)
 
 export default Clients
--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import styles, { colors_gradient } from "../constants/style";
+import { arrowUp } from "../assets";
 
-function GetStarted() {
-  return (
-    <div>
-      GetStarted
+const GetStarted = () => (
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-gradient-to-t from-emerald-400 to-white p-[2px] cursor-pointer hover:p-[4px]`}
+  >
+    <div
+      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+    >
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+          <span className={`${colors_gradient.textGradientMain}`}>Get</span>
+        </p>
+
+        <img
+          src={arrowUp}
+          alt="arrow"
+          className="w-[23px] h-[23px] object-contain"
+        />
+      </div>
+
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <span className={`${colors_gradient.textGradientMain}`}>Started</span>
+      </p>
     </div>
-  )
-}
+  </div>
+);
 
-export default GetStarted
+export default GetStarted;
